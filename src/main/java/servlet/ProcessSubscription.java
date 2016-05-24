8ac9f1d25a2b58f0114976a45413efbfbd78168f
snehashish <panigrahisnehasis@gmail.com>
2016-05-25 03:24:55 +0530

    Fixed compilation exception

--- src/main/java/servlet/ProcessSubscription.java
+++ src/main/java/servlet/ProcessSubscription.java
@@ -3,6 +3,7 @@ package servlet;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -27,14 +28,22 @@ public class ProcessSubscription extends HttpServlet {
 //		https://graph.facebook.com//oauth/access_token?client_id=<app-id>&client_secret=<app-secret>&grant_type=client_credentials
 //		https://graph.facebook.com//oauth/access_token?client_id=521073994746729&client_secret=b6e08769dc3d3421f9677855ba852013&grant_type=client_credentials
 		String url = "https://graph.facebook.com//oauth/access_token?client_id="+ app_Id +"&client_secret=" + app_Secret + "&grant_type=client_credentials";
-		URL urldemo = new URL(url);
-		URLConnection uc = urldemo.openConnection();
-		BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
-		String inputLine;
-		while ((inputLine = in.readLine()) != null)
-			System.out.println("app accessToken : "+inputLine);
-		app_AccessToken = inputLine;
-		in.close();  
+		try {
+			URL urldemo = new URL(url);
+			URLConnection uc = urldemo.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+				System.out.println("app accessToken : "+inputLine);
+			app_AccessToken = inputLine;
+			in.close();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
 	}
 	
 	@Override
@@ -53,14 +62,19 @@ public class ProcessSubscription extends HttpServlet {
 		
         if(app_AccessToken != null && page_acessToken != null){
 //        	https://graph.facebook.com/debug_token?input_token=<page-short-live-access-token>&access_token=<app-accesstoken>
-        	String url = "https://graph.facebook.com/debug_token?input_token="+ page_acessToken +"&access_token=" + app_AccessToken;
-    		URL urldemo = new URL(url);
-    		URLConnection uc = urldemo.openConnection();
-    		BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
-    		String inputLine;
-    		while ((inputLine = in.readLine()) != null)
-    			System.out.println("debug the page access token"+inputLine);
-    		in.close(); 
+        	try {
+				String url = "https://graph.facebook.com/debug_token?input_token="+ page_acessToken +"&access_token=" + app_AccessToken;
+				URL urldemo = new URL(url);
+				URLConnection uc = urldemo.openConnection();
+				BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+				String inputLine;
+				while ((inputLine = in.readLine()) != null)
+					System.out.println("debug the page access token"+inputLine);
+				in.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
         }
 
 /*        response.setContentType("text/html;charset=UTF-8");
